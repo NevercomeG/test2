6b6a5e4b14786356852ba58fcd0fd252fd72fdb3
NevercomeG <luisjavier2012@gmail.com>
2021-08-01 21:24:07 -0400

    9

--- Query.py
+++ Query.py
@@ -1,24 +1,4 @@
-import requests
-import hashlib
-import datetime
+import django
 from connection import connection
-import flask
+
 database = connection()
-
-def Creating_new_user(database):
-    try:
-        with database.atomic():
-            # Attempt to create the user. If the username is taken, due to the
-            # unique constraint, the database will raise an IntegrityError.
-            user = flask.User.create(
-                username=requests.form['username'],
-                password=hashlib.md5(requests.form['password']).hexdigest(),
-                email=requests.form['email'],
-                join_date=datetime.datetime.now())
-
-        # mark the user as being 'authenticated' by setting the session vars
-        flask.auth_user(user)
-        return flask.redirect('homepage')
-
-    except flask.IntegrityError:
-        flask('That username is already taken')
